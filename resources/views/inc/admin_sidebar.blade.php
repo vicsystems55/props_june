1fb5d8c08cd694fbb0f7c1d8aebbe4bc9495c2d9
vicSystems <victorasuquob@gmail.com>
2021-07-30 16:04:56 +0100

    search property

--- resources/views/inc/admin_sidebar.blade.php
+++ resources/views/inc/admin_sidebar.blade.php
@@ -5,10 +5,7 @@
 	    	<li class="treeview {{ (request()->is('admin/home')) ? 'active' : '' }}"><a class="" href="{{route('admin.home')}}"><i class="flaticon-layers "></i><span> Dashboard</span></a></li>
 	      	<li class="treeview {{ (request()->is('admin/members')) ? 'active' : '' }}"><a href="{{route('admin.members')}}"><i class="flaticon-envelope"></i><span> Members</span></a></li>
 	   		<li class="title"><span>Manage Listings</span></li>
-	      	<li class="treeview {{ (request()->is( 'admin/listings', 
-		
-			  
-			  )) ? 'active' : '' }}">
+	      	<li class="treeview ">
 		        <a href="#"><i class="flaticon-home"></i> <span>Properties</span><i class="fa fa-angle-down pull-right"></i></a>
 		        <ul class="treeview-menu">
 		        	<li class="{{ (request()->is('admin/my_listings')) ? 'active' : '' }}"><a href="{{route('admin.listings')}}"><i class="fa fa-circle"></i>Listings</a></li>
